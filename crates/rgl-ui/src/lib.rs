2fe38be221d261edb9da4ae7bfbefa6e675b5084
bakane <dyatlovleonid@gmail.com>
2023-12-15 10:26:49 +0300

    added commands ext

--- crates/rgl-ui/src/lib.rs
+++ crates/rgl-ui/src/lib.rs
@@ -1,9 +1,9 @@
-use bevy::{ecs::system::EntityCommands, prelude::*, transform::commands};
+use bevy::{ecs::system::EntityCommands, prelude::*};
 
 pub trait UiCommandsExt<'w, 's, T: Component> {
     fn make_button(
         &mut self,
-        text: &str,
+        text: impl Into<String>,
         style: Style,
         marker: T,
         asset_server: Res<'w, AssetServer>,
@@ -13,17 +13,30 @@ pub trait UiCommandsExt<'w, 's, T: Component> {
 impl<'w, 's, T: Component> UiCommandsExt<'w, 's, T> for Commands<'w, 's> {
     fn make_button(
         &mut self,
-        text: &str,
+        text: impl Into<String>,
         style: Style,
         marker: T,
         asset_server: Res<'w, AssetServer>,
     ) -> EntityCommands<'w, 's, '_> {
         let mut ec = self.spawn((
             ButtonBundle {
-                image: UiImage::new(asset_server.get_handle("buttons/menu/"))..Default::default(),
+                image: UiImage::new(
+                    asset_server
+                        .get_handle("buttons/menu/generic_button_normal.png")
+                        .unwrap(),
+                ),
+                style,
+                ..Default::default()
             },
             marker,
         ));
+
+        ec.with_children(|parent| {
+            parent.spawn(TextBundle {
+                text: Text::from_section(text, TextStyle::default()),
+                ..Default::default()
+            });
+        });
 
         ec
     }
--- examples/button.rs
+++ examples/button.rs
@@ -1,11 +1,10 @@
 use bevy::prelude::*;
 use bevy_pixel_camera::{PixelCameraPlugin, PixelViewport};
-use rgl_ui::UiPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins((UiPlugin, PixelCameraPlugin))
+        .add_plugins(PixelCameraPlugin)
         .add_systems(Startup, setup)
         .run();
 }
